765e0f6251183832bfea0d23a8826ee683af0471
geonhwiii <geonhwiii@users.noreply.github.com>
2026-02-04 01:00:17 +0900

    refact: ThumbnailSection 내 이미지 추상화

--- src/pages/ProductDetailPage/components/ThumbnailSection.tsx
+++ src/pages/ProductDetailPage/components/ThumbnailSection.tsx
@@ -9,7 +9,7 @@ const ParamsSchema = z.object({
   id: z.string(),
 });
 
-function ThumbnailSection() {
+export default function ThumbnailSection() {
   const { id } = ParamsSchema.parse(useParams());
   const [selectedImageIndex, setSelectedImageIndex] = useState(0);
 
@@ -20,38 +20,15 @@ function ThumbnailSection() {
 
   return (
     <styled.section css={{ bg: 'background.01_white' }}>
-      <styled.img
-        src={images[selectedImageIndex]}
-        alt={'product-main-image'}
-        css={{
-          w: 'full',
-          aspectRatio: 1,
-          objectFit: 'cover',
-          bg: 'background.01_gray',
-        }}
-      />
-
-      {/* 썸네일 이미지 */}
+      <MainThumbnailImage src={images[selectedImageIndex]} alt={'product-main-image'} />
       <HStack gap={2} css={{ overflowX: 'auto', justifyContent: 'center', pt: 2 }}>
         {images.map((image, index) => (
-          <styled.img
-            key={`thumbnail-${index}`}
+          <ThumbnailItem
+            key={index}
             src={image}
             alt={`product-image-${index + 1}`}
+            isSelected={selectedImageIndex === index}
             onClick={() => setSelectedImageIndex(index)}
-            role="button"
-            css={{
-              w: 12,
-              h: 12,
-              objectFit: 'cover',
-              cursor: 'pointer',
-              outline: selectedImageIndex === index ? '1px solid' : 'none',
-              outlineColor: 'primary.01_primary',
-              outlineOffset: selectedImageIndex === index ? '-1px' : 0,
-              rounded: 'md',
-              overflow: 'hidden',
-              flexShrink: 0,
-            }}
           />
         ))}
       </HStack>
@@ -59,4 +36,50 @@ function ThumbnailSection() {
   );
 }
 
-export default ThumbnailSection;
+function MainThumbnailImage({ src, alt }: { src: string; alt: string }) {
+  return (
+    <styled.img
+      src={src}
+      alt={alt}
+      css={{
+        w: 'full',
+        aspectRatio: 1,
+        objectFit: 'cover',
+        bg: 'background.01_gray',
+      }}
+    />
+  );
+}
+
+function ThumbnailItem({
+  src,
+  alt,
+  isSelected,
+  onClick,
+}: {
+  src: string;
+  alt: string;
+  isSelected: boolean;
+  onClick: () => void;
+}) {
+  return (
+    <styled.img
+      src={src}
+      alt={alt}
+      onClick={onClick}
+      role="button"
+      css={{
+        w: 12,
+        h: 12,
+        objectFit: 'cover',
+        cursor: 'pointer',
+        outline: isSelected ? '1px solid' : 'none',
+        outlineColor: 'primary.01_primary',
+        outlineOffset: isSelected ? '-1px' : 0,
+        rounded: 'md',
+        overflow: 'hidden',
+        flexShrink: 0,
+      }}
+    />
+  );
+}
